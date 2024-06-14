977a254184583b98750427de33784a76878ee38a
pvrkchan <132545516+pvrkchan@users.noreply.github.com>
2024-06-14 14:57:02 +0900

    Update BottomNavigationBar.kt

--- compose/B09_WQGA/app/src/main/java/com/example/b09_wqga/component/BottomNavigationBar.kt
+++ compose/B09_WQGA/app/src/main/java/com/example/b09_wqga/component/BottomNavigationBar.kt
@@ -5,13 +5,18 @@
 package com.example.b09_wqga.component
 
 import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Icon
 import androidx.compose.material3.NavigationBar
 import androidx.compose.material3.NavigationBarItem
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import androidx.navigation.NavGraph.Companion.findStartDestination
 import androidx.navigation.compose.currentBackStackEntryAsState
@@ -35,10 +40,12 @@ fun BottomNavigationBar(navController: NavController, userId: String) {
                         restoreState = true
                     }
                 },
+                alwaysShowLabel = false,
                 icon = {
-                    Icon(
-                        imageVector = if (currentRoute == navItem.route) navItem.onSelectedIcon else navItem.selectIcon,
-                        contentDescription = stringResource(id = navItem.titleId)
+                    Image(
+                        painter = painterResource(navItem.selectIcon),
+                        contentDescription = stringResource(id = navItem.titleId),
+                        modifier = Modifier.size(30.dp)
                     )
                 },
                 label = {
