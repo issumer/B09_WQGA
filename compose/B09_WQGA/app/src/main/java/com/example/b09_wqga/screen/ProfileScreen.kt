25d98ac6bd4ccea8a776ee93284d0ef3cf589b35
pvrkchan <132545516+pvrkchan@users.noreply.github.com>
2024-06-14 18:29:44 +0900

    Update ProfileScreen.kt

--- compose/B09_WQGA/app/src/main/java/com/example/b09_wqga/screen/ProfileScreen.kt
+++ compose/B09_WQGA/app/src/main/java/com/example/b09_wqga/screen/ProfileScreen.kt
@@ -1,21 +1,25 @@
 package com.example.b09_wqga.screen
 
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.Logout
 import androidx.compose.material.icons.filled.Info
 import androidx.compose.material3.Button
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
@@ -26,6 +30,9 @@ import androidx.navigation.NavController
 import androidx.navigation.NavHostController
 import com.example.b09_wqga.R
 import com.example.b09_wqga.model.UserDataViewModel
+import com.example.b09_wqga.ui.theme.nanumFontFamily
+import com.example.b09_wqga.ui.theme.pixelFont1
+import com.example.b09_wqga.ui.theme.pixelFont2
 import com.example.b09_wqga.viewmodel.UserViewModel
 
 @Composable
@@ -58,15 +65,16 @@ fun ProfileScreen(userId: String, userViewModel: UserViewModel, navController: N
             horizontalArrangement = Arrangement.SpaceBetween
         ) {
             Image(
-                painter = painterResource(id = R.drawable.ic_launcher_foreground),
+                painter = painterResource(id = R.drawable.accounticon),
                 contentDescription = "Profile Picture",
-                modifier = Modifier.size(64.dp)
+                modifier = Modifier.size(50.dp)
             )
             Column {
-                Text(text = "ID: $username", fontSize = 20.sp, fontWeight = FontWeight.Bold)
-                Text(text = "Name: $name", fontSize = 16.sp)
+                Text(text = "ID: $username", fontSize = 20.sp, fontFamily = pixelFont2, fontWeight = FontWeight.ExtraBold)
+                Spacer(modifier = Modifier.height(4.dp))
+                Text(text = "Name: $name", fontSize = 20.sp,fontFamily = pixelFont2, fontWeight = FontWeight.ExtraBold)
             }
-            Button(onClick = {
+            TextButton(onClick = {
                 userViewModel.logout(context) {
                     navController.navigate("LoginScreen") {
                         userDataViewModel.showBottomNavigationBar.value = false
@@ -75,10 +83,18 @@ fun ProfileScreen(userId: String, userViewModel: UserViewModel, navController: N
                         }
                     }
                 }
-            }) {
-                Icon(imageVector = Icons.AutoMirrored.Filled.Logout, contentDescription = "Log out")
-                Spacer(modifier = Modifier.width(4.dp))
-                Text("Log out")
+            },
+                modifier = Modifier
+                    .background(
+                        color = Color.Black,
+                        shape = RoundedCornerShape(10.dp)
+                    )
+                    .padding(start = 5.dp, end = 5.dp)) {
+                Icon(painter = painterResource(id = R.drawable.logout), contentDescription = "Log out",
+                modifier = Modifier.size(20.dp), tint = Color.White
+                    )
+                Spacer(modifier = Modifier.width(8.dp))
+                Text("Log out", fontFamily = pixelFont1, color = Color.White)
             }
         }
 
@@ -88,6 +104,7 @@ fun ProfileScreen(userId: String, userViewModel: UserViewModel, navController: N
         Text(
             text = "Information",
             fontSize = 24.sp,
+            fontFamily = pixelFont1,
             fontWeight = FontWeight.Bold,
             modifier = Modifier.padding(bottom = 16.dp)
         )
@@ -105,7 +122,7 @@ fun ProfileScreen(userId: String, userViewModel: UserViewModel, navController: N
             Row(verticalAlignment = Alignment.CenterVertically, modifier = Modifier.padding(vertical = 8.dp)) {
                 Icon(imageVector = Icons.Default.Info, contentDescription = "Info Icon", modifier = Modifier.size(24.dp))
                 Spacer(modifier = Modifier.width(8.dp))
-                Text(text = info, fontSize = 16.sp)
+                Text(text = info, fontSize = 16.sp, fontFamily = pixelFont2)
             }
         }
 
