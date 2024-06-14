cccd3e964c6b97c94f872c4a5720f3fcbf210c67
pvrkchan <132545516+pvrkchan@users.noreply.github.com>
2024-06-14 10:25:44 +0900

    폰트 추가

---
+++ compose/B09_WQGA/app/src/main/java/com/example/b09_wqga/ui/theme/Font.kt
@@ -0,0 +1,13 @@
+package com.example.b09_wqga.ui.theme
+
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import com.example.b09_wqga.R
+
+val nanumFontFamily = FontFamily(
+    Font(R.font.nanum_square_l, FontWeight.Light),
+    Font(R.font.nanum_square_r, FontWeight.Normal),
+    Font(R.font.nanum_square_b, FontWeight.Bold),
+    Font(R.font.nanum_square_eb, FontWeight.ExtraBold),
+)
