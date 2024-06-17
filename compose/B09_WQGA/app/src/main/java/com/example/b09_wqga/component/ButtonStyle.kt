33e5a2b2f0fa9b3aaacd55906122562da81e1af8
pvrkchan <132545516+pvrkchan@users.noreply.github.com>
2024-06-18 00:42:32 +0900

    Update ButtonStyle.kt

--- compose/B09_WQGA/app/src/main/java/com/example/b09_wqga/component/ButtonStyle.kt
+++ compose/B09_WQGA/app/src/main/java/com/example/b09_wqga/component/ButtonStyle.kt
@@ -3,11 +3,13 @@ package com.example.b09_wqga.component
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.text.font.FontWeight
@@ -23,7 +25,7 @@ fun Button_WQGA(width: Int, height: Int, text: String, onClickLabel : () -> Unit
             .then(if (enabled) Modifier.clickable { onClickLabel() } else Modifier)
             .background(
                 color = if (enabled) Color.Black else Color.Gray,
-                shape = RoundedCornerShape((height / 2).dp)
+                shape = RoundedCornerShape((height / 2 - 6).dp)
             )
             .size(width = width.dp, height = height.dp),
 //            .clickable { onClickLabel() },
@@ -34,7 +36,7 @@ fun Button_WQGA(width: Int, height: Int, text: String, onClickLabel : () -> Unit
             text = text,
             fontWeight = FontWeight.ExtraLight,
             fontFamily = pixelFont1,
-            fontSize = 14.sp,
+            fontSize = 13.sp,
             color = Color.White
         )
     }
