0087357269e91954629b002a83b0e8ff2924723e
pvrkchan <132545516+pvrkchan@users.noreply.github.com>
2024-06-18 00:07:33 +0900

    Update SearchBar.kt

--- compose/B09_WQGA/app/src/main/java/com/example/b09_wqga/component/SearchBar.kt
+++ compose/B09_WQGA/app/src/main/java/com/example/b09_wqga/component/SearchBar.kt
@@ -4,41 +4,74 @@
 
 package com.example.b09_wqga.component
 
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.Canvas
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Search
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.SolidColor
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.b09_wqga.R
+import com.example.b09_wqga.ui.theme.pixelFont2
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun SearchBar(searchText: String, onSearchTextChanged: (String) -> Unit) {
     Row(
         modifier = Modifier.width(280.dp),
         verticalAlignment = Alignment.CenterVertically
     ) {
-        Icon(imageVector = Icons.Default.Search, contentDescription = "Search Icon")
-
         Spacer(modifier = Modifier.width(8.dp))
 
         OutlinedTextField(
             value = searchText,
+            placeholder = {
+                Text(text = "Enter PW", color = Color.Gray, fontSize = 14.sp, fontFamily = pixelFont2, fontWeight = FontWeight.Normal)
+            },
+            shape = RoundedCornerShape(25.dp),
+            leadingIcon = { Icon(painter = painterResource(R.drawable.search), contentDescription = null,
+                modifier = Modifier.size(25.dp)) },
             onValueChange = onSearchTextChanged,
-            label = { Text("Enter Search") },
             modifier = Modifier.weight(1f),
             keyboardOptions = KeyboardOptions.Default.copy(
                 imeAction = ImeAction.Search,
                 keyboardType = KeyboardType.Text
+            ),
+            colors = TextFieldDefaults.outlinedTextFieldColors(
+                focusedBorderColor = Color.Black,
+                unfocusedBorderColor = Color.Transparent
             )
         )
     }
